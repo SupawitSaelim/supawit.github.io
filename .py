036564b9220b2767edd6e19b03d8032ca3e4da8c
SupawitSaelim <s6506022420053@email.kmutnb.ac.th>
2022-09-24 17:09:27 +0700

    check

--- .py
+++ .py
@@ -1,10 +1,8 @@
 print('\n\n\n')
 
-
-
-university = "king Mongkut's University technology north bangkok"
-print(university.replace("north", "south"))
-
+i = input(" ")
+print(i.isalnum())
+print(i.isalpha())
 
 
 
