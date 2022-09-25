5e4cfc88a53351c7b86b4bd588353d086e3aad16
SupawitSaelim <s6506022420053@email.kmutnb.ac.th>
2022-09-25 17:36:21 +0700

    check

--- .py
+++ .py
@@ -1,15 +1,3 @@
-from unittest import result
-
-
-def main():
-    i = open('text.txt', 'r')
-    
-    readdata = i.readline()
-
-    while readdata != '':
-        print(readdata.rstrip('\n'))
-        readdata = i.readline()
-
-main()
-
-print('\n\n\n')
+import os
+print('aklsdflasjdf')
+os.system("clear")
