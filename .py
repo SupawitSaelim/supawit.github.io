e7a362e27f0d343db677ba0f78188d8f8a348946
SupawitSaelim <s6506022420053@email.kmutnb.ac.th>
2022-09-25 19:08:20 +0700

    check

--- .py
+++ .py
@@ -1,3 +1,19 @@
-import os
-print('aklsdflasjdf')
-os.system("clear")
+def main():
+    i = input('Enter file name to read: ')
+
+    try:
+        fileread = open(i, 'r')
+        sum = 0
+
+        for i in fileread:
+            sum += int(i)
+        fileread.close()
+    except Exception as e:
+        print(e)
+        exit()
+    else:
+        print(sum)
+    finally:
+        print('Done!!!')    
+
+main()
