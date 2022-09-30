32e230c65be7b8e49e48e683d10fdb3c5477b64e
SupawitSaelim <s6506022420053@email.kmutnb.ac.th>
2022-09-30 21:37:30 +0700

    check

--- .py
+++ .py
@@ -1,18 +1,20 @@
-import matplotlib.pyplot as plt
+import pickle as pkl
 
 def main():
-    x_axis = [1,2,3,4,5]
-    y_axis = [2,5,2,1,3]
+    with open('data.dat','rb') as file:
+        end_file = False
 
-    plt.plot(x_axis,y_axis,color='red')
-    plt.title('Sale of year')
-    plt.xlabel('Year')
-    plt.ylabel('Sale')
+        while not end_file:
+            try:
+                data = pkl.load(file)
+                display_data(data)
+            except EOFError:
+                end_file = True
 
-    plt.yticks([1,2,3,4,5],['$1M','$2M','$3M','$4M','$5M'])
-    plt.xticks(x_axis,['2020','2021','2022','2023','2024'])
-
-    plt.grid(True)
-    plt.show()
+def display_data(data):
+    print('Name: ', data['name'])
+    print('Age: ', data['age'])
+    print('Address: ', data['address'])
+    print()
 
 main()
