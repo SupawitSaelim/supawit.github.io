a1fb45d885c505be4ce2c426945fb0e673909a97
SupawitSaelim <s6506022420053@email.kmutnb.ac.th>
2022-10-01 16:03:00 +0700

    check

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
-values = 57
-if values >50: print("greater than 50")
-else: print("less than 50")
+score_of_the_player = int(input("Enter a score of the test -1 to stop: "))   
+total_score= 0
+scorecount= 0                       
+
+while score_of_the_player != -1:                     
+    total_score = total_score + score_of_the_player
+    scorecount= scorecount + 1
+    score_of_the_player = int(input("Enter a score of the test -1 to stop: "))  
+print ("The average for the test is ",total_score/scorecount)
