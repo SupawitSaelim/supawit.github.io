a8ff72f6989fb782546cafa32667b7d736caa11e
SupawitSaelim <s6506022420053@email.kmutnb.ac.th>
2022-10-01 17:03:42 +0700

    check

--- test.py
+++ test.py
@@ -1,9 +1,4 @@
-score_of_the_player = int(input("Enter a score of the test -1 to stop: "))   
-total_score= 0
-scorecount= 0                       
+list_text = 'supawit sleep in class programming'.capitalize()
+print(list_text)
 
-while score_of_the_player != -1:                     
-    total_score = total_score + score_of_the_player
-    scorecount= scorecount + 1
-    score_of_the_player = int(input("Enter a score of the test -1 to stop: "))  
-print ("The average for the test is ",total_score/scorecount)
+
