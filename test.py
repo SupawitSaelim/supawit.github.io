d3dc2e70832283168e41cbf16713b822b3c0621c
SupawitSaelim <s6506022420053@email.kmutnb.ac.th>
2022-10-01 22:59:26 +0700

    check

--- test.py
+++ test.py
@@ -1,6 +1,53 @@
-tuple1 = 1, 2, "Hello"  # packing variables into tuple
-print(tuple1)  
-
-i, j, k = tuple1 # unpacking tuple into variable
-print(i, j, k) 
-
+<h2><br><br>SOME DICTIONARY METHOD <img src="lec7/3459188.png" width="70" alt=""> </h2>
+								<div class="table-wrapper">
+									<table class="alt">
+										<thead>
+											<tr>
+												<th>Method</th>
+												<th>Description</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td>clear()</td>
+												<td>Removes all the elements from the dictionary</td>
+											</tr>
+											<tr>
+												<td>copy()</td>
+												<td>Returns a copy of the dictionary</td>
+											</tr>
+											<tr>
+												<td>get()</td>
+												<td>Returns the value of the specified key</td>
+											</tr>
+											<tr>
+												<td>items()</td>
+												<td>Returns a list containing a tuple for each key value pair</td>
+											</tr>
+											<tr>
+												<td>keys()</td>
+												<td>Returns a list containing the dictionary's keys</td>
+											</tr>
+											<tr>
+												<td>pop()</td>
+												<td>Removes the element with the specified key</td>
+											</tr>
+											<tr>
+												<td>popitem()</td>
+												<td>Removes the last inserted key-value pair</td>
+											</tr>
+											<tr>
+												<td>setdefault()</td>
+												<td>Returns the value of the specified key. If the key does not exist: insert the key, with the specified value</td>
+											</tr>
+											<tr>
+												<td>update()</td>
+												<td>Updates the dictionary with the specified key-value pairs</td>
+											</tr>
+											<tr>
+												<td>values()</td>
+												<td>Returns a list of all the values in the dictionary</td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
